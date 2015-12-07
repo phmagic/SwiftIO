00682a2b215e7536771e442fc2692ccdcc617249
Jonathan Wight <jwight@mac.com>
2015-12-07 11:44:36 -0800

    Fixup travis

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,12 +2,10 @@
 
 export PATH=$PATH:/usr/local/bin
 
-brew install carthage
-
-carthage bootstrap
-
-xcodebuild -version | grep "Xcode 7" > /dev/null || { echo 'Not running Xcode 7' ; exit 1; }
-
 cd `git rev-parse --show-toplevel`
 
+brew list xctool || brew install xctool
+brew list carthage || brew install carthage
+carthage bootstrap
+xcodebuild -version | grep "Xcode 7" > /dev/null || { echo 'Not running Xcode 7' ; exit 1; }
 xctool -project SwiftIO.xcodeproj -scheme All build test || exit $!
