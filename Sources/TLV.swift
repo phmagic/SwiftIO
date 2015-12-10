29a208df1c02944f767de90e6a7780ef65138a8f
Jonathan Wight <jwight@mac.com>
2015-12-10 11:38:22 -0800

    Flesh out TLV - still a work in progress.

--- Sources/TLV.swift
+++ Sources/TLV.swift
@@ -48,7 +48,68 @@ extension TLVRecord: BinaryInputStreamable {
      }
 }
 
+extension DispatchData {
+
+    func split <T> () throws -> (T, DispatchData) {
+        let (left, right) = split(sizeof(T))
+        let value = left.createMap() {
+            (data, buffer) in
+            return (buffer.toUnsafeBufferPointer() as UnsafeBufferPointer <T>)[0]
+        }
+        return (value, right)
+
+    }
+
+}
+
+
+
+
+public extension TLVRecord {
+
+    static func read(data: DispatchData <Void>, endianess: Endianess) throws -> (TLVRecord, DispatchData <Void>) {
+        let (type, data): (Type, DispatchData <Void>) = try data.split()
+        let (length, data2): (Length, DispatchData <Void>) = try data.split()
+
+
+
+
+
+
+        let length2 = Int(length.toIntMax())
+
+
+        let (data3, data4) = data2.split(length2)
+        let record = TLVRecord(type: type, data: data3)
+        return (record, data4)
+    }
+
+     static func read(data: DispatchData <Void>, endianess: Endianess) throws -> ([TLVRecord], DispatchData <Void>) {
+        var records: [TLVRecord] = []
+        typealias Record = TLVRecord <UInt16, UInt16>
+        var remainingData = data
+        while remainingData.length > 0 {
+            let record: TLVRecord
+            (record, remainingData) = try! read(data, endianess: endianess)
+            records.append(record)
+        }
+        return (records, remainingData)
+    }
+    
+}
+
 // MARK: -
+
+public extension TLVRecord {
+    func toDispatchData() throws -> DispatchData <Void> {
+        let length = Length(UIntMax(self.data.length))
+        let data = DispatchData <Void> ()
+            + DispatchData <Void> (value: type)
+            + DispatchData <Void> (value: length)
+            + self.data
+        return data
+    }
+}
 
 extension TLVRecord: BinaryOutputStreamable {
     public func writeTo <Target: BinaryOutputStream> (stream: Target) throws {
