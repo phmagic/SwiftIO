72aef139d7db7f56544a16a650005d92ea531e31
Jonathan Wight <jwight@mac.com>
2015-12-11 17:04:38 -0800

    Better testing of TCP streams.

--- TestApp/ServerViewController.swift
+++ TestApp/ServerViewController.swift
@@ -19,6 +19,11 @@ class ServerViewController: NSViewController {
     let port = UInt16(40000 + SwiftUtilities.random.random(uniform: 1000))
     var server: Server!
     var clientChannel: TCPChannel!
+    var count: Int = 0
+
+    dynamic var log: String = ""
+
+    dynamic var connected: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +31,46 @@ class ServerViewController: NSViewController {
         try! startServer()
         try! startClient()
     }
+
+}
+
+extension ServerViewController {
+
+    @IBAction func connect(sender: AnyObject?) {
+        clientChannel.connect() {
+            (result) in
+            self.log_debug(result)
+        }
+    }
+
+    @IBAction func disconnect(sender: AnyObject?) {
+        clientChannel.disconnect() {
+            (result) in
+            self.log_debug(result)
+        }
+    }
+
+    @IBAction func ping(sender: AnyObject?) {
+        let record = try! Record(type: 100, data: DispatchData <Void> ("Hello world: \(count++)"))
+        let data = try! record.toDispatchData(self.endianess)
+        self.clientChannel!.write(data) {
+            (result) in
+            self.log_debug(result)
+        }
+    }
+
+}
+
+
+extension String {
+    init(data: DispatchData <Void>, encoding: NSStringEncoding = NSUTF8StringEncoding) throws {
+        let nsdata = data.toNSData()
+        self = NSString(data: nsdata, encoding: encoding) as! String
+    }
+
+}
+
+extension ServerViewController {
 
     func startServer() throws {
         let address = try Address(address: "localhost", `protocol`: InetProtocol.TCP, family: ProtocolFamily.INET)
@@ -45,10 +90,13 @@ class ServerViewController: NSViewController {
 
                     let (records, remaining): ([Record], DispatchData <Void>) = try! Record.read(buffer, endianess: self.endianess)
                     for record in records {
-                        print("**** \(record)")
+
+                        dispatch_async(dispatch_get_main_queue()) {
+                            let string = try! String(data: record.data)
+                            self.log_debug(result)
+                        }
                     }
                     buffer = remaining
-                    print(buffer)
                 }
             }
         }
@@ -59,14 +107,31 @@ class ServerViewController: NSViewController {
 
         clientChannel = try TCPChannel(hostname: "localhost", port: port)
 
+        clientChannel.stateChangeCallback = {
+            (old, new) in
+
+            switch (old, new) {
+                case (_, .Unconnected):
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.connected = false
+                    }
+                case (_, .Connected):
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.connected = true
+                    }
+                default:
+                    break
+            }
+        }
+
         clientChannel.readCallback = {
             (result) in
             if let error = result.error {
                 print(error)
                 return
             }
-            if let data = result.value {
-            }
+//            if let data = result.value {
+//            }
         }
 
         clientChannel.connect() {
@@ -74,18 +139,24 @@ class ServerViewController: NSViewController {
 
             if let error = result.error {
                 assert(self.clientChannel.state == .Unconnected)
-                print("Connection failure: \(error)")
+                self.log_debug(error)
                 return
             }
 
             for _ in 0..<2 {
-                let record = try! Record(type: 100, data: DispatchData <Void> ("Hello world"))
+                let record = try! Record(type: 100, data: DispatchData <Void> ("Hello world: \(self.count++)"))
                 let data = try! record.toDispatchData(self.endianess)
                 self.clientChannel!.write(data) {
                     (result) in
-                    print("Write: ", result)
+                    self.log_debug(result)
                 }
             }
+        }
+    }
+
+    func log_debug(value: Any) {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.log += String(value) + "\n"
         }
     }
 }
