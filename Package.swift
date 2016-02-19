7ade35af308f64c7e7bf377960aca365d4c2eff8
Jonathan Wight <jwight@mac.com>
2016-02-19 10:37:24 -0800

    Request latest (as of today) SwiftUtilities.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftIO",
     dependencies: [
-        .Package(url: "https://github.com/schwa/SwiftUtilities.git", versions: Version(0,0,27)..<Version(0,1,0)),
+        .Package(url: "https://github.com/schwa/SwiftUtilities.git", majorVersion: 0),
     ]
 )
-
