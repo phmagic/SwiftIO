214335ea437ea9a8b1d6f7fa3da0091752c0f266
Jonathan Wight <jwight@mac.com>
2016-05-06 12:04:09 -0700

    Clean up imports - get merge superfluous files.

--- Sources/Inet+Utilities.swift
+++ Sources/Inet+Utilities.swift
@@ -29,7 +29,6 @@
 //  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Darwin
-import Foundation
 
 import SwiftUtilities
 
@@ -160,4 +159,30 @@ public func getaddrinfo(hostname: String, service: String, hints: addrinfo, bloc
         current = current.memory.ai_next
     }
     freeaddrinfo(info)
+}
+
+// MARK: -
+
+public extension in_addr {
+    var octets: (UInt8, UInt8, UInt8, UInt8) {
+        let address = UInt32(networkEndian: s_addr)
+        return (
+            UInt8((address >> 24) & 0xFF),
+            UInt8((address >> 16) & 0xFF),
+            UInt8((address >> 8) & 0xFF),
+            UInt8(address & 0xFF)
+        )
+    }
+}
+
+public extension in6_addr {
+    var words: (UInt16, UInt16, UInt16, UInt16, UInt16, UInt16, UInt16, UInt16) {
+        assert(sizeof(in6_addr) == sizeof((UInt16, UInt16, UInt16, UInt16, UInt16, UInt16, UInt16, UInt16)))
+        var copy = self
+        return withUnsafePointer(&copy) {
+            let networkWords = UnsafeBufferPointer <UInt16> (start: UnsafePointer <UInt16> ($0), count: 8)
+            let words = networkWords.map() { UInt16(networkEndian: $0) }
+            return (words[0], words[1], words[2], words[3], words[4], words[5], words[6], words[7])
+        }
+    }
 }
